66e8ef6023749bf22066b0a943aa5a479ddf6ea0
serhii <sergas123456@gmail.com>
2026-02-02 17:34:52 +0200

    upd

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  output: "export",
   images: {
     qualities: [25, 50, 75, 80, 100]
   }
