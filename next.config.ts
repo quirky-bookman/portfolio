de6ec44b70aa8f84ab196002854475f7004a822c
serhii <sergas123456@gmail.com>
2026-02-02 17:46:57 +0200

    updt

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const repoName = "portfolio";
+const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
+  basePath: isProduction ? `/${repoName}` : "",
+  assetPrefix: isProduction ? `/${repoName}/` : "",
   output: "export",
   images: {
     unoptimized: true
